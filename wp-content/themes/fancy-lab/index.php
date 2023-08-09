f3d36ad8cd94a9d5df1a71311f2eb86ae5d55b8f
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-09 15:49:33 +0600

    5.2_the basic blocks of a wordpress theme-p2_b_s2_t12

--- wp-content/themes/fancy-lab/index.php
+++ wp-content/themes/fancy-lab/index.php
@@ -1,23 +1,20 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="profile" href="https://gmpg.org/xfn/11" />
-        
-</head>
-<body <?php body_class();?>>
-    <div id="page" class="site">
-        <header>
-            <section class="search">Search</section>
-            <section class="top-bar">
-                <div class="brand">Logo</div>
-                <div class="second-column">
-                    <div class="account">Account</div>
-                    <nav class="main-menu">Menu</nav>
-                </div>
-            </section>
-        </header>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package  Fancy Lab
+ */
+
+get_header();
+?>
+
         <div class="content-area">
              <main>
                 <section class="slider">Slider</section>
@@ -27,11 +24,5 @@
                 <section class="lab-blog">News</section>
              </main>
         </div>
-        <footer>
-            <section class="footer-widgets">Footer Widgets</section>
-            <section class="copyright">Copyright</section>
-        </footer>
-
-    </div>
-</body>
-</html>
+ 
+<?php get_footer(); ?>
