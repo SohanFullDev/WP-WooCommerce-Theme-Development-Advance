9e2d2f61614cf0223493db6bc6a2b1ab40a1bfd6
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-23 10:02:10 +0600

    10_including bootstrap (part2)_s2t16

--- wp-content/themes/fancy-lab/header.php
+++ wp-content/themes/fancy-lab/header.php
@@ -22,12 +22,23 @@
 <body <?php body_class();?>>
     <div id="page" class="site">
         <header>
-            <section class="search">Search</section>
-            <section class="top-bar">
-                <div class="brand">Logo</div>
-                <div class="second-column">
-                    <div class="account">Account</div>
-                    <nav class="main-menu">Menu</nav>
+            <section class="search">
+                <div class="container">
+                    Search
+
                 </div>
+            </section>
+            <section class="top-bar">
+                <div class="container">
+                    <div class="row">
+                    <div class="brand col-3">Logo</div>
+                            <div class="second-column col-9">
+                                <div class="account">Account</div>
+                                <nav class="main-menu">Menu</nav>
+                            </div>
+                    
+                    </div>
+                </div>
+               
             </section>
         </header>
