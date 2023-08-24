ade62aa7aadaecb805e61504e2183b41754e3498
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-24 12:24:58 +0600

    14_the wordpress loop (part2)_s2t20

--- wp-content/themes/fancy-lab/index.php
+++ wp-content/themes/fancy-lab/index.php
@@ -40,7 +40,25 @@ get_header();
                 <section class="lab-blog">
                         <div class="container">
                                 <div class="row">
-                                News
+                               <?php 
+                               //if there are any posts
+                               if(have_posts() ):
+
+                                //load posts loop
+                                while( have_posts() ): the_post();
+                                ?>
+                                <article>
+                                        <h2><?php the_title(); ?></h2>
+                                        <div><?php the_content(); ?></div>
+
+                                </article>
+                                <?php
+                               endwhile;
+                             else:
+                         ?>
+                         <p>Nothing to display</p>
+                         <?php endif; ?>
+
                                 </div>
                         </div>
                 </section>
